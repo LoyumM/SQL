5f756915f34a84ed526551a279d516cf2f16b3b6
LoyumM <lmbmoirangthem033@gmail.com>
2023-04-03 18:09:59 +0530

    update select from select

--- Sqlzoo/SELECT FROM SELECT.sql
+++ Sqlzoo/SELECT FROM SELECT.sql
@@ -12,9 +12,9 @@ WHERE name='Russia');
 SELECT name
 FROM world
 WHERE continent = 'Europe'
-    AND gdp/population > (
+  AND gdp/population > (
   SELECT gdp/population
-    FROM world
-    WHERE name = 'United Kingdom');
+  FROM world
+  WHERE name = 'United Kingdom');
 
 --3.
