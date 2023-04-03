eb39720580840e3a228df1e3b8bc672fdc42dbd0
Loyumba Moirangthem <lmbmoirangthem033@gmail.com>
2023-04-03 18:01:27 +0530

    Update SELECT FROM NOBEL.sql

--- Sqlzoo/SELECT FROM NOBEL.sql
+++ Sqlzoo/SELECT FROM NOBEL.sql
@@ -76,7 +76,7 @@ SELECT *
 FROM nobel
 WHERE winner = 'EUGENE O\'
 NEILL';
-'
+
 -- 13.Knights in order. List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
 
 SELECT winner, yr, subject
