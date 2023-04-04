874f51a4654be678f806cfc85b1115c833757414
LoyumM <lmbmoirangthem033@gmail.com>
2023-04-04 13:26:48 +0530

    sum and count

--- Sqlzoo/SUM and COUNT.sql
+++ Sqlzoo/SUM and COUNT.sql
@@ -26,3 +26,22 @@ SELECT SUM(population) AS total_population
 FROM world
 WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
 
+-- 6.For each continent show the continent and number of countries.
+
+SELECT continent, COUNT(name) AS total_country
+FROM world
+GROUP BY continent;
+
+-- 7.For each continent show the continent and number of countries with populations of at least 10 million.
+
+SELECT continent, COUNT(name) AS total_countries
+FROM world
+WHERE population >= 10000000
+GROUP BY continent;
+
+-- 8.List the continents that have a total population of at least 100 million.
+
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000;
