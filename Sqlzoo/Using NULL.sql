7d5f8efc40d52cfed750d2a00fbfaa5577391b2c
LoyumM <lmbmoirangthem033@gmail.com>
2023-04-05 14:57:03 +0530

    using null

--- Sqlzoo/Using NULL.sql
+++ Sqlzoo/Using NULL.sql
@@ -48,3 +48,22 @@ FROM teacher RIGHT JOIN dept
     ON teacher.dept = dept.id
 GROUP BY dept.name;
 
+-- 9.Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+
+SELECT name,
+    CASE WHEN dept IN (1,2)
+      THEN 'Sci'
+      ELSE 'Art'
+   END AS dept
+FROM teacher;
+
+-- 10.Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+
+SELECT name,
+    CASE WHEN dept IN (1,2)
+      THEN 'Sci'
+      WHEN dept = 3
+      THEN 'Art'
+      ELSE 'None'
+   END AS dept
+FROM teacher;
