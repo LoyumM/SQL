140889c1b8ca15b0146df2c8cfb2e7f5b39a5861
LoyumM <lmbmoirangthem033@gmail.com>
2023-06-08 13:07:36 +0530

    y-on-y growth rate

---
+++ DataLemur/Hard/Y-on-Y growth rate.sql
@@ -0,0 +1,14 @@
+WITH
+    cte
+    AS
+    (
+        SELECT EXTRACT(year FROM transaction_date) AS year,
+            product_id,
+            spend AS curr_year_spend,
+            LAG(spend,1) OVER(PARTITION BY product_id) AS prev_year_spend
+        FROM user_transactions
+    )
+
+SELECT year, product_id, curr_year_spend, prev_year_spend,
+    ROUND(100.0*(curr_year_spend-prev_year_spend)/prev_year_spend,2) AS yoy_rate
+FROM cte;
