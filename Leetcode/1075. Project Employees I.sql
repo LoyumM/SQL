90886d5a6bab2738a28e526d4a3047cca82e257c
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-04 17:17:44 +0530

    avg exp sql

---
+++ Leetcode/1075. Project Employees I.sql
@@ -0,0 +1,6 @@
+-- # Write your MySQL query statement below
+SELECT project_id,
+    ROUND(AVG(experience_years),2) AS average_years
+FROM Project p LEFT JOIN Employee e
+    ON p.employee_id = e.employee_id
+GROUP BY project_id
