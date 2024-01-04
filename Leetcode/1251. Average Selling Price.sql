dd7dd7690d27fe985b1f901f928ae87e9313c6db
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-04 15:26:11 +0530

    solve null

--- Leetcode/1251. Average Selling Price.sql
+++ Leetcode/1251. Average Selling Price.sql
@@ -1,8 +1,9 @@
 -- Write your MySQL query statement below
 SELECT p.product_id,
     ROUND(SUM(units * price)/SUM(units),2) AS average_price
-FROM Prices p 
-    CROSS JOIN UnitsSold u
-WHERE p.product_id = u.product_id
-    AND purchase_date BETWEEN start_date AND end_date
+FROM Prices p
+    LEFT JOIN UnitsSold u
+    ON p.product_id = u.product_id
+        AND u.purchase_date 
+        BETWEEN start_date AND end_date
 GROUP BY p.product_id;
