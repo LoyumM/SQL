6281d8fa5f816226b1c5cf538d84e7cde259dce1
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-04 17:43:25 +0530

    percentage attendence

---
+++ Leetcode/1633. Percentage of Users Attended a Contest.sql
@@ -0,0 +1,7 @@
+-- # # Write your MySQL query statement below
+SELECT contest_id,
+    ROUND(COUNT(distinct user_id) * 100 /(SELECT COUNT(user_id)
+    FROM Users) ,2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC,contest_id;
