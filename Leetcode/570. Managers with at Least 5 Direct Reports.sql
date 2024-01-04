592fd41f73a1dcc9a4110c4032063a34e05192dc
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-04 12:54:35 +0530

    faster solution for manager with 5 reports

--- Leetcode/570. Managers with at Least 5 Direct Reports.sql
+++ Leetcode/570. Managers with at Least 5 Direct Reports.sql
@@ -13,3 +13,14 @@ SELECT name
 FROM Employee m LEFT JOIN Reports r
     ON m.id = r.managerId
 WHERE employees >= 5;
+
+
+
+-- Write your MySQL query statement below
+SELECT name
+FROM Employee
+WHERE id IN 
+    (SELECT managerId
+FROM Employee
+GROUP BY managerId
+HAVING COUNT(managerId) >=5 );
