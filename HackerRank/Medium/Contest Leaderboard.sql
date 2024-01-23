643cdabd298d608236dac0ee8b4918dd678eb7e4
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-23 14:09:02 +0530

    contest leaderboard

---
+++ HackerRank/Medium/Contest Leaderboard.sql
@@ -0,0 +1,26 @@
+/*
+Enter your query here.
+*/
+
+WITH
+    cte
+    AS
+    
+    (
+        SELECT
+            s.hacker_id,
+            name,
+            challenge_id,
+            MAX(score) AS score
+        FROM submissions as s
+            JOIN hackers as h on h.hacker_id=s.hacker_id
+        GROUP BY s.hacker_id, name, challenge_id
+    )
+
+SELECT hacker_id,
+    name,
+    SUM(score) AS total_score
+FROM cte
+WHERE score > 0
+GROUP BY hacker_id, name
+ORDER BY 3 DESC, 1;
