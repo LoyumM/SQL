39b0c013795c191eb628b8ba2cddd7998ec46fbe
LoyumM <lmbmoirangthem033@gmail.com>
2024-01-23 13:32:29 +0530

    works

--- HackerRank/Medium/Top competitors.sql
+++ HackerRank/Medium/Top competitors.sql
@@ -1,21 +1,15 @@
-WITH
-    cte
-    AS
-    
-    (
-        SELECT
-            h.hacker_id,
-            h.name,
-            COUNT(DISTINCT s.challenge_id) AS full_scores
-        FROM Hackers h
-            LEFT JOIN Submissions s ON h.hacker_id = s.hacker_id
-            LEFT JOIN Challenges c ON s.challenge_id = c.challenge_id
-            LEFT JOIN Difficulty d ON c.difficulty_level = d.difficulty_level
-        WHERE s.score = d.score
-        GROUP BY 1, 2
-        ORDER BY 3 DESC, 1 ASC
-    )
-
-SELECT CONCAT(hacker_id, ' ', name) AS id_name
-FROM cte
-WHERE full_scores > 1;
+SELECT
+    h.hacker_id
+    , h.name
+FROM hackers as h
+    JOIN submissions as s on h.hacker_id=s.hacker_id
+    JOIN challenges as c on s.challenge_id=c.challenge_id
+    JOIN difficulty as d ON c.difficulty_level=d.difficulty_level
+WHERE s.score=d.score
+GROUP BY 
+    h.hacker_id
+    , h.name
+HAVING COUNT(h.hacker_id) >=2
+ORDER BY 
+    COUNT(h.hacker_id) DESC
+    , h.hacker_id ASC;
